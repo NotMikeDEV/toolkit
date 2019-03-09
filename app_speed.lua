ca8b08d47cddb344ee6f0986386046084ba85e58
NotMike <notmike@notmike.co.uk>
2019-03-09 07:57:13 +0000

    Change IP/speed test URLs

--- app_speed.lua
+++ app_speed.lua
@@ -1,12 +1,11 @@
 #!/usr/local/sbin/container
 require("module/network")
 require("module/caddy")
-require("module/php")
 local NIC = network:AddInterface{type='ethernet', name='net0', default_route=true}
 NIC:AddIP{ipv4='44.131.14.81', ipv6='2a07:1c44:260e::81', nat=true}
 
 function AddProxies(Website)
-	Website:AddProxy{source='/dns/', target="http://127.0.0.1:8080"}
+	Website:AddProxy{source='/whoami/', target="http://127.0.0.1:8080"}
 	Website:AddProxy{source='/speed/', target="http://127.0.0.1:8080"}
 end
 local ToolkitAS206671 = caddy:AddWebsite{hostname='', port=80, root='/tools'}
--- whoami.js
+++ whoami.js
@@ -12,18 +12,16 @@ module.exports = {
 			setTimeout(function(){
 				return SendReply(500, 'text/plain', "Server Timeout. (WhoAmI API)");
 			},5000);
-			var IPAddress = req.headers['x-real-ip'];
+			var IPAddress = req.headers['cf-connecting-ip']?req.headers['cf-connecting-ip']:req.headers['x-real-ip'];
 			if (IPAddress.substr(0,7) == '::ffff:')
 				IPAddress = IPAddress.substr(7);
 			return execBG('dig -x ' + IPAddress + ' +short|head -n 1', function(err, stdout){
-				var Reply = "<p><b>Closest Server: " + fs.readFileSync("/etc/hostname") + "</b></br>";
+				var Reply = "<p><b>Closest Server:</b> " + fs.readFileSync("/etc/hostname") + "</br>";
 				Reply += "<b>IP Address: "+ IPAddress + "</b>\n";
 				if (stdout && stdout != IPAddress)
 					Reply += "<br><b>Hostname:</b> " + stdout + "\n";
-				if (IPAddress.includes(":"))
-					Reply += '<br><a href="http://ipv6.speed.as206671.uk/speed/">[ IPv6 Speed Test ]</a></p>';
-				else
-					Reply += '<br><a href="http://ipv4.speed.as206671.uk/speed/">[ IPv4 Speed Test ]</a></p>';
+				Reply += '<br><a href="http://ipv6.speed.as206671.uk/speed/">[ IPv6 Speed Test ]</a>';
+				Reply += '<br><a href="http://ipv4.speed.as206671.uk/speed/">[ IPv4 Speed Test ]</a></p>';
 				console.log(Reply);
 				SendReply(200, 'text/html', Reply);
 			});
